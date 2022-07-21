716ccaf1085b970fbafa7d65020926ceff3c969a
Aleksandr <cifrotech@yandex.ru>
2022-07-21 22:50:55 +1000

    full complete

--- src/components/common/spoiler/spoiler.ts
+++ src/components/common/spoiler/spoiler.ts
@@ -9,25 +9,22 @@ export default class Spoiler extends Component {
 
 	clickHandler = (e: Event) => {
 		
-		const spoiler = (<HTMLElement>e.target)
-		const parent = <HTMLElement>spoiler.parentNode
+		const spoiler = <HTMLElement>e.target
+		const parent = <HTMLElement>spoiler.closest('.spoiler')
 		const id = parseInt(parent.dataset.spoiler)
-
 		const allSpoilers = document.querySelectorAll('[data-spoiler]')
 		const aciveSpoiler = allSpoilers[id]
 
-		console.log(aciveSpoiler)
 
         if (spoiler) {
 
 			const collapse = aciveSpoiler.querySelector('.spoiler__collapse') as HTMLElement
-			const icon = aciveSpoiler.querySelector('.spoiler__head-icon') as HTMLElement
+
 
 			if (aciveSpoiler.classList.contains('active')) {
 				aciveSpoiler.classList.remove('active')
 				collapse.style.height = '0px'
 			} else {
-				// allSpoilers.forEach(item => item.classList.remove('active'))
 				aciveSpoiler.classList.add('active')
 				collapse.style.height = `${collapse.scrollHeight}px`
 			}
