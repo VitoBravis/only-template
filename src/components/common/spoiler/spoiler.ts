18c2b6bd1f50f6c251df3cdc7d3359ca077a10cd
Aleksandr <cifrotech@yandex.ru>
2022-07-21 17:21:32 +1000

    collapse height % oder px?!

--- src/components/common/spoiler/spoiler.ts
+++ src/components/common/spoiler/spoiler.ts
@@ -3,9 +3,37 @@ import Component, { ComponentProps } from '@/base/component';
 export default class Spoiler extends Component {
     constructor(element: ComponentProps) {
         super(element);
+		  this.nRoot.addEventListener('click', this.clickHandler);
+		  
     }
 
-    destroy = () => {
-        // Destroy functions
+	clickHandler = (e: Event) => {
+		const spoiler = (<HTMLElement>e.target).closest('.spoiler__head')
+		
+        if (spoiler) {
+
+			const collapse = document.querySelector('.spoiler__collapse') as HTMLElement
+			const icon = document.querySelector('.spoiler__head-icon') as HTMLElement
+
+			console.log(collapse.offsetHeight)
+
+			if (icon.classList.contains('active')) {
+				icon.classList.remove('active')
+				collapse.style.height = '0px'
+			} else {
+				icon.classList.add('active')
+				collapse.style.height = `${collapse.scrollHeight}px`
+			}
+
+
+			
+			
+
+			
+			
+			
+		  }
     }
+
+	
 }
--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,10 +1,14 @@
 import { ITransitionData } from '@barba/core/dist/core/src/defs';
+import Spoiler from '@/components/common/spoiler/spoiler';
+import { getComponent } from '@/helpers/helpers';
 
 export default {
     namespace: 'common',
     async beforeEnter({ next }: ITransitionData) {
         try {
             // Инициализация компонентов
+				new Spoiler(getComponent('spoiler'))
+
         } catch (e) {
             console.error(e);
         }
