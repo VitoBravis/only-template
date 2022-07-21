e0f1eb020556adf8fe28c68b66404cefe878cf3c
Ivan <Bospur@mail.ru>
2022-07-21 14:45:21 +0500

    fixed open spoiler on click collapse

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -1,24 +1,30 @@
-import Component, { ComponentProps } from '@/base/component';
+import Component, { ComponentProps } from "@/base/component";
 
 export default class Spoiler extends Component {
     constructor(element: ComponentProps) {
         super(element);
 
-        this.nRoot.addEventListener('click', this.clickHandler)
+        this.nRoot.addEventListener("click", this.clickHandler);
     }
 
     clickHandler = (e: Event) => {
-        const collapse = this.nRoot.querySelector('.spoiler__collapse') as any;
+        const target = e.target as HTMLElement;
+        const head = target?.closest(".spoiler__head");
+        const collapse = this.nRoot.querySelector(
+            ".spoiler__collapse"
+        ) as HTMLElement;
 
-        this.nRoot.classList.toggle('active');
-        const isActive = this.nRoot.classList.contains('active');
+        if (head) {
+            this.nRoot.classList.toggle("active");
+            const isActive = this.nRoot.classList.contains("active");
 
-        setTimeout(() => {
-            if (isActive) {
-                collapse.style.height = `${collapse.scrollHeight}px`;
-            } else {
-                collapse.style.height = 0;
-            }
-        }, 300)
-    }
+            setTimeout(() => {
+                if (isActive) {
+                    collapse.style.height = `${collapse.scrollHeight}px`;
+                } else {
+                    collapse.style.height = "0";
+                }
+            }, 300);
+        }
+    };
 }
