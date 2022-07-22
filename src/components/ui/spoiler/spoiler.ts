6a570765b1608498b60c1cff2cb82afb47a38b76
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2022-07-22 13:52:55 +0300

    Task 1, first try

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -3,9 +3,24 @@ import Component, { ComponentProps } from '@/base/component';
 export default class Spoiler extends Component {
     constructor(element: ComponentProps) {
         super(element);
+
+        this.nRoot.addEventListener('click', this.clickHandler);
     }
 
-    destroy = () => {
-        // Destroy functions
+    clickHandler = (e: Event) => {
+        const spoilerTitle = this.nRoot.firstElementChild as HTMLElement
+        const spoilerText = this.nRoot.children[1] as HTMLElement
+        const isOpen = spoilerTitle.classList.contains('spoiler__title_open');
+
+        spoilerTitle.classList.toggle('spoiler__title_open');
+        
+        if(isOpen) {
+            spoilerText.style.height = '0';
+        } else {spoilerText.style.height = `${spoilerText.scrollHeight}px`};
+
+
+
     }
+
+    
 }
