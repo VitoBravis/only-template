f1f5b430424eea3418948fddfe0956089d391c16
bospur <bospobob@yandex.ru>
2022-07-25 15:55:45 +0500

    fixed

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -1,30 +1,29 @@
 import Component, { ComponentProps } from "@/base/component";
 
 export default class Spoiler extends Component {
+    isOpen: boolean;
+    collapse: HTMLElement;
+
     constructor(element: ComponentProps) {
         super(element);
-
+        this.isOpen = false;
+        this.collapse = this.getElement('collapse')!;
         this.nRoot.addEventListener("click", this.clickHandler);
     }
 
     clickHandler = (e: Event) => {
-        const target = e.target as HTMLElement;
-        const head = target?.closest(".spoiler__head");
-        const collapse = this.nRoot.querySelector(
-            ".spoiler__collapse"
-        ) as HTMLElement;
-
-        if (head) {
-            this.nRoot.classList.toggle("active");
-            const isActive = this.nRoot.classList.contains("active");
-
-            setTimeout(() => {
-                if (isActive) {
-                    collapse.style.height = `${collapse.scrollHeight}px`;
-                } else {
-                    collapse.style.height = "0";
-                }
-            }, 300);
-        }
+        this.isOpen ? this.close() : this.open();
     };
+
+    close = () => {
+        this.isOpen = false;
+        this.nRoot.classList.remove("active");
+        this.collapse.style.height = "0";
+    }
+
+    open = () => {
+        this.isOpen = true;
+        this.nRoot.classList.add("active");
+        this.collapse.style.height = `${this.collapse.scrollHeight}px`
+    }
 }
