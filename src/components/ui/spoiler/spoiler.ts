c4866a8f37a0e7ee1c21854f6e4145d03c33effc
Pavel <pavel.krasinskiy@bk.ru>
2022-07-25 18:09:21 +0800

    Changes

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -17,15 +17,12 @@ export default class Spoiler extends Component {
             .children[1] as HTMLElement;
         const spoilerTitle = spoilerElement.children[0]
             .children[0] as HTMLElement;
+
         spoilerIcon.classList.toggle("active");
-        if (spoilerIcon.classList.contains("active")) {
-            spoilerCollapse.style.height = `${spoilerCollapse.scrollHeight}px`;
-            spoilerIcon.style.transform = "rotate(45deg)";
-            spoilerTitle.style.color = "#02818A";
-        } else {
-            spoilerCollapse.style.height = "0px";
-            spoilerIcon.style.transform = "rotate(-45deg)";
-            spoilerTitle.style.color = "#012B34;";
-        }
+        const iconIsActive = spoilerIcon.classList.contains("active");
+        spoilerCollapse.style.height = `${
+            !!iconIsActive ? spoilerCollapse.scrollHeight : 0
+        }px`;
+        spoilerTitle.style.color = !!iconIsActive ? "#02818A" : "#012B34";
     };
 }
