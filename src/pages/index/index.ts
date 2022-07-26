7223782d4b6772088cfca8a2fcb97453f27e1d3b
Maksim Haikevich <mhaikevich@gmail.com>
2022-07-27 00:24:55 +0300

    first task (Maksim Haikevich)

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,10 +1,13 @@
 import { ITransitionData } from '@barba/core/dist/core/src/defs';
+import Spoiler from "@/components/ui/spoiler/spoiler";
+import {getComponent} from "@/helpers/helpers";
 
 export default {
     namespace: 'common',
     async beforeEnter({ next }: ITransitionData) {
         try {
             // Инициализация компонентов
+            const spoilerComponent = new Spoiler(getComponent('spoiler'));
         } catch (e) {
             console.error(e);
         }
