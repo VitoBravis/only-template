5e8a8a1df761fbf55e46ca24f3f61bef86448e1f
Dedam4 <shtrak2014@mail.ru>
2022-07-26 11:11:57 +0300

    Elimination of comments

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -1,31 +1,26 @@
 import Component, { ComponentProps } from '@/base/component';
 
 export default class Spoiler extends Component {
+    spoilerItems: HTMLElement[];
+
     constructor(element: ComponentProps) {
         super(element);
+      
 
-
+        
+        this.spoilerItems = this.getElements('item');
+        this.nRoot.addEventListener('click', this.handlerClick);
         // this.nRoot.children[1].childNodes.forEach(el => {el.addEventListener('click', this.handlerClick);})
 
-        this.nRoot.addEventListener('click', this.handlerClick);
     }
 
 
 
     handlerClick = (e: Event) => {
 
-
-        const spoilers = document.querySelectorAll('.spoiler__item');
-        const btn = (<HTMLElement>e.target);
-
-
-        console.log(btn)
-
-
-
         let content;
-        spoilers.forEach(element => {
-            if (element === btn.closest('.spoiler__item' ) || element === btn.closest('.spoiler__btn') || element === btn.closest('.spoiler__item-title')) {
+        this.spoilerItems.forEach(element => {
+            if (element.contains(e.target) ) {
 
                 content = element.children[1];
                 this.spollerBody(element, content);
@@ -36,22 +31,12 @@ export default class Spoiler extends Component {
     }
     spollerBody(spoilersItem: any, content: any) {
 
-
-
         if (spoilersItem.classList.contains('_active')) {
             spoilersItem.classList.remove('_active');
-
             content.style.maxHeight = '0px'
-
-
-
-
         } else {
             spoilersItem.classList.add('_active')
-
             content.style.maxHeight = `${content.scrollHeight}px`
-
-
 
         }
 
