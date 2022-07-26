6a27d89b0fa0ee319ecb10a7a813f5e5bafdea78
Aleksandr <cifrotech@yandex.ru>
2022-07-26 16:37:07 +1000

    the end

--- src/components/common/spoiler/spoiler.ts
+++ src/components/common/spoiler/spoiler.ts
@@ -3,12 +3,14 @@ import Component, { ComponentProps } from '@/base/component';
 export default class Spoiler extends Component {
 
 	collapse: HTMLElement;
+	icon: HTMLElement;
 	isOpen: boolean;
 
     constructor(element: ComponentProps) {
         super(element);
 		  this.isOpen = false;
         this.collapse = this.getElement('collapse')!;
+		  this.icon = this.getElement('head-icon')!;
 		  this.nRoot.addEventListener('click', this.clickHandler);
     }
 
@@ -19,11 +21,15 @@ export default class Spoiler extends Component {
 	 close = () => {
 		this.isOpen = false;
 		this.collapse.style.height = '0px'
+		this.collapse.classList.remove('active')
+		this.icon.classList.remove('active')
 	 }
 
 	 open = () => {
 		this.isOpen = true;
 		this.collapse.style.height = `${this.collapse.scrollHeight}px`
+		this.collapse.classList.add('active')
+		this.icon.classList.add('active')
 	 }
 
 	
