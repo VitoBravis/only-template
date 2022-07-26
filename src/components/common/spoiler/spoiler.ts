b39de37d96197eca64b7d7ee60f1264c64083f3b
Aleksandr <cifrotech@yandex.ru>
2022-07-26 13:06:49 +1000

    rebuild spoiler open/close mechanizm

--- src/components/common/spoiler/spoiler.ts
+++ src/components/common/spoiler/spoiler.ts
@@ -1,36 +1,30 @@
 import Component, { ComponentProps } from '@/base/component';
 
 export default class Spoiler extends Component {
+
+	collapse: HTMLElement;
+	isOpen: boolean;
+
     constructor(element: ComponentProps) {
         super(element);
+		  this.isOpen = false;
+        this.collapse = this.getElement('collapse')!;
 		  this.nRoot.addEventListener('click', this.clickHandler);
-		  
     }
 
-	clickHandler = (e: Event) => {
-		
-		const spoiler = <HTMLElement>e.target
-		const parent = <HTMLElement>spoiler.closest('.spoiler')
-		const id = parseInt(parent.dataset.spoiler)
-		const allSpoilers = document.querySelectorAll('[data-spoiler]')
-		const aciveSpoiler = allSpoilers[id]
-
-
-        if (spoiler) {
-
-			const collapse = aciveSpoiler.querySelector('.spoiler__collapse') as HTMLElement
-
-
-			if (aciveSpoiler.classList.contains('active')) {
-				aciveSpoiler.classList.remove('active')
-				collapse.style.height = '0px'
-			} else {
-				aciveSpoiler.classList.add('active')
-				collapse.style.height = `${collapse.scrollHeight}px`
-			}
-
-		  }
+	clickHandler = () => {
+		this.isOpen ? this.close() : this.open()
     }
+
+	 close = () => {
+		this.isOpen = false;
+		this.collapse.style.height = '0px'
+	 }
+
+	 open = () => {
+		this.isOpen = true;
+		this.collapse.style.height = `${this.collapse.scrollHeight}px`
+	 }
 
 	
 }
