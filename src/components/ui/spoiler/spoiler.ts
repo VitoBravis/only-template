f56cfebabe2efbb2f2142e53c482e3426d8c2962
Cry0ne <ss421299@gmail.com>
2022-07-28 17:16:41 +0300

    * Исправил media-quary в scss; * Заменил </hr> на правило бордер у элементов; * Добавил проверку на наличие элемента 'spoiler'; * Отделил контент от pug; ** Прочее: * Пределал логику spoilerWrapper и spoiler: * Теперь spoilerWrapper <ul> элемент и содержит внутри each в котором каждому компоненту spoiler передается контент конкретного споилера.

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -1,22 +1,19 @@
 import Component, { ComponentProps } from '@/base/component';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { resize } from '../../../app/js/helpers/helpers';
 
 export default class Spoiler extends Component {
 
-    
     constructor(element: ComponentProps) {
         super(element);
-        this.nRoot.addEventListener('click', this.clickHandler);        
+        this.nRoot.children[0].addEventListener('click', this.clickHandler);        
     } 
 
     clickHandler = (e:Event) =>{
-        const spoilerElement = (<HTMLElement>e.target).closest('.spoiler__element');
-        if(!spoilerElement) return;
+        const spoiler = (<HTMLElement>e.target).closest('.spoiler');
+        if(!spoiler) return;
 
-        spoilerElement.classList.toggle("is-open");
-        const spoilerCollapse = spoilerElement.children[1] as HTMLElement;
-        spoilerCollapse.style.height = spoilerElement.classList.contains('is-open') 
+        spoiler.classList.toggle("is-open");
+        const spoilerCollapse = spoiler.children[1] as HTMLElement;
+        spoilerCollapse.style.height = spoiler.classList.contains('is-open') 
             ? `${spoilerCollapse.scrollHeight}px` 
             : spoilerCollapse.style.height = '0';
     }
