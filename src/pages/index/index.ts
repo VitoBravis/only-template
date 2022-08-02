ddf87be349c45929e3f096ac91ceccc1a0a47514
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2022-08-02 21:04:56 +0300

    merge

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -3,9 +3,9 @@ import { ITransitionData } from '@barba/core/dist/core/src/defs';
 import Swiper from 'swiper';
 
 
-//import Spoiler from '@/components/ui/spoiler/spoiler';
-//import Counter from '@/components/sections/counter/counter';
-//import Button from '@/components/ui/button/button';
+import Spoiler from '@/components/ui/spoiler/spoiler';
+import Counter from '@/components/sections/counter/counter';
+import Button from '@/components/ui/button/button';
 
 
 export default {
@@ -15,12 +15,12 @@ export default {
             const btn = getComponent('button')
             const counter = getComponent('counter')
 
-            //btn.component && new Button(btn)
-            //counter.component && new Counter(counter)
+            btn.component && new Button(btn)
+            counter.component && new Counter(counter)
 
-            //if (getComponent('spoiler', next.container).component) {
-            //    getComponents('spoiler', next.container).map((item) => new Spoiler(item));
-            //}
+            if (getComponent('spoiler', next.container).component) {
+                getComponents('spoiler', next.container).map((item) => new Spoiler(item));
+            }
         } catch (e) {
             console.error(e);
         }
